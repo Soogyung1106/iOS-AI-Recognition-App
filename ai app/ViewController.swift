5da84f40051646b9ea064db9f3144733e29cec9c
Soogyung Jung <sally1106@naver.com>
2024-01-31 00:55:21 +0900

    .git파일 삭제

---
+++ ai app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ai app
+//
+//  Created by 정수경 on 2024/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
